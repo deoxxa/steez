7aba4dc9eda57f5d96df31423de46f9f134e561e
deoxxa <deoxxa@fknsrs.biz>
2012-10-19 15:59:20 +1100

    completely rewrote everything 3 times. head hurts.

--- index.js
+++ index.js
@@ -12,28 +12,15 @@ var Steez = module.exports = function Steez() {
   this.closed = false;
 
   this.queue = [];
-}
-
+};
 util.inherits(Steez, stream.Stream);
 
-// hijacking emit() to re-route "data" events
 Steez.prototype._emit = Steez.prototype.emit;
 Steez.prototype.emit = function emit() {
-  // hijack data events
   if (arguments[0] === "data") {
-    // if we're closing or are already closed, we don't want to let any data
-    // events through at all.
-    if (this.closing || this.closed) {
-      return this;
-    }
-
-    // this is to facilitate skipping the whole queueing process if possible.
-    // we can be marked as not paused but still have data in the queue in some
-    // circumstances, so we have to check the queue length.
-    if (this.paused || this.queue.length) {
-      this.queue.push(arguments[1]);
-      return this;
-    }
+    this.queue.push(arguments[1]);
+    this.flush();
+    return this;
   }
 
   return this._emit.apply(this, arguments);
@@ -45,85 +32,102 @@ Steez.prototype.write = function write(data) {
   return this.writable;
 };
 
-Steez.prototype.end = function end(data) {
-  if (data) {
-    this.write(data);
-  }
-
-  return this.destroySoon();
-};
-
-Steez.prototype.destroySoon = function destroySoon() {
-  // mark as not writable.
-  this.writable = false;
-
-  // mark as closing - this stops "data" events from working so we don't
-  // accumulate more data in the queue.
-  this.closing = true;
-
-  // if we've still got data in the queue, we can't destroy() yet so we schedule
-  // another attempt at everything later on.
-  if (this.queue.length !== 0) {
-    return this.once("drain", this.destroySoon.bind(this));
-  }
-
-  // switch from closing to closed - this stops resume() from working
-  this.closing = false;
-  this.closed = true;
-
-  // we can be sure no more data events are going to be emitted, so it's time to
-  // emit "end"
-  this.emit("end");
-
-  // close everything down
-  return this.destroy();
-};
-
-Steez.prototype.destroy = function destroy() {
-  // mark as not readable/writable
-  this.readable = false;
-  this.writable = false;
-
-  // empty the write queue
-  this.queue.splice(0);
-
-  // emit "close" to tell everything listening that we've gone away
-  return this.emit("close");
-};
-
 Steez.prototype.pause = function pause() {
-  // mark as not writable
-  this.writable = false;
-
-  // mark as paused - this toggles data queueing and resume() functionality
   this.paused = true;
+  this.writable = false;
 
   return this;
 };
 
 Steez.prototype.resume = function resume() {
-  // we only want to resume if we're not closed and we're currently paused
-  if (!this.closed && this.paused) {
-    this.paused = false;
-
-    // inside flush_queue, we emit "drain" when necessary
-    this.flush_queue();
+  if (this.closed) {
+    return this;
   }
 
-  return this;
+  this.paused = false;
+
+  return this.flush();
 };
 
-Steez.prototype.flush_queue = function flush_queue() {
+Steez.prototype.flush = function flush() {
+  if (this.closed) {
+    return this;
+  }
+
   while (this.queue.length && !this.paused) {
     this._emit("data", this.queue.shift());
   }
 
-  // we emit "drain" when we've drained the write buffer *and* we're not in the
-  // process of shutting everything down.
-  if (this.queue.length === 0 && !this.closing) {
-    this.writable = true;
-    this._emit("drain");
+  if (this.queue.length === 0) {
+    if (this.closing) {
+      this.destroySoon();
+    } else if (!this.writable) {
+      this.writable = true;
+      this.emit("drain");
+    }
   }
 
   return this;
 };
+
+Steez.prototype.end = function end(data) {
+  if (data) {
+    this.write(data);
+  }
+
+  this.writable = false;
+  this.closing = true;
+
+  return this.flush();
+};
+
+Steez.prototype.can_destroy = function can_destroy() {
+  return this.queue.length === 0;
+};
+
+Steez.prototype.destroySoon = function destroySoon() {
+  if (this.queue.length) {
+    return this.flush();
+  }
+
+  if (this.queue.length) {
+    return this.once("drain", this.destroySoon.bind(this));
+  }
+
+  if (!this.can_destroy()) {
+    process.nextTick(this.destroySoon.bind(this));
+    return this;
+  }
+
+  if (!this.closed) {
+    this.destroy();
+  }
+};
+
+Steez.prototype.destroy = function() {
+  this.writable = false;
+  this.readable = false;
+  this.closed = true;
+
+  this.queue.splice(0);
+
+  this.emit("end");
+  this.emit("close");
+};
+
+// lol super debugging
+/*
+for (var k in Steez.prototype) {
+  if (!Object.hasOwnProperty.apply(Steez.prototype, [k])) {
+    continue;
+  }
+
+  (function(k) {
+    var fn = Steez.prototype[k];
+    Steez.prototype[k] = function() {
+      console.log(this.constructor.name, k, arguments);
+      return fn.apply(this, arguments);
+    };
+  }(k));
+}
+*/
